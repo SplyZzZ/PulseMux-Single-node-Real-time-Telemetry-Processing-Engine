7e438990a7754a32805ad742bdc08e2be2030499
Stas Grechaniuk <dedsv18@gmail.com>
2026-01-05 20:09:11 +0000

    Worker

--- src/pipeline/worker.cpp
+++ src/pipeline/worker.cpp
@@ -1,1 +1,35 @@
 #include "pipeline/worker.h"
+#include <thread>
+#include <queue>
+#include <mutex>
+#include "parse_validate/event.h"
+void Worker::run()
+{
+    while(start_)
+    {
+        Event ev;
+        {
+            std::lock_guard<std::mutex> lock(workerMutex_);
+            if(task_.empty()) { continue;}
+            ev = std::move(task_.front());
+            task_.pop();
+        }
+        //TODO : робота воркера
+    }
+    
+   
+}
+void Worker::set(Event&& event)
+{
+   std::lock_guard<std::mutex> lock(workerMutex_);
+   task_.emplace(std::move(event));
+}
+Worker::~Worker()
+{
+    start_ = false;
+    if(worker_.joinable)
+    {
+        worker_.join();
+    }
+    
+}
